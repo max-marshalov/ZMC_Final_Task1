fb18fcd6afab266bcdbf9bb1fb095fce0c675f35
Geralt from Rivia <ctvbxm@mail.ru>
2021-03-16 13:17:29 +0200

    готовые вход и регистрация начало заполения анкеты

--- main.py
+++ main.py
@@ -95,17 +95,76 @@ class Reg(QtWidgets.QMainWindow):
 
 
 class Zap_Step1(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, person=None):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_Zap_Step1()
         self.ui.setupUi(self)
 
+        self.person = person
+
+        self.ui.btn_next.clicked.connect(self.go_next)
+
+        self.ui.btn_load_photo.clicked.connect(self.load_photo)
+
+    def load_photo(self):
+        self.file_path = QtWidgets.QFileDialog.getOpenFileName(self, 'headers', 'filename')
+
+    def go_next(self):
+        radio_base_sex = [self.ui.radioButton_female, self.ui.radioButton_male]
+        Surname = self.ui.edit_surname.text()
+        Name = self.ui.edit_name.text()
+        Otch = self.ui.edit_dad.text()
+        Sex = None
+        for i in radio_base_sex:
+            if i.isChecked():
+                Sex = i.text()
+        Login = self.ui.edit_mail.text()
+        Date = self.ui.edit_date.text()
+        Phone = self.ui.edit_phone.text()
+        Birth_place = self.ui.edit_birth_place.text()
+        Life = None
+        Photo = self.file_path
+        radio_base_life = [self.ui.radioButton_yes, self.ui.radioButton_no]
+        for i in radio_base_life:
+            if i.isChecked():
+                Life = i.text()
+
+        id = self.person[0][0]
+
+        con = sqlite3.connect("DATABASE.db")
+        curs = con.cursor()
+        curs.execute(
+            f"""UPDATE UserForm SET name = "{Name}", surname = "{Surname}", otchestvo = "{Otch}", sex = "{Sex}", email = "{Login}", birthday = "{Date}", place_of_birth = "{Birth_place}", phone_number = "{Phone}", photo_path = "{Photo}", campus = "{Life}" WHERE id = "{id}" """)  # если не будет работать, убери кавычки на id
+        con.commit()
+        con.close()
+
+        try:
+            self.win = Zap_Step2(self, person=self.person)
+            self.close()
+            self.win.show()
+
+        except Exception as er:
+            print(er)
+
 
 class Zap_Step2(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, person=None):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_Zap_Step2()
         self.ui.setupUi(self)
+
+        self.person = person
+
+        self.ui.btn_next.clicked.connect(self.go_next)
+
+    def go_next(self):
+        try:
+            self.win = Zap_Step3(self, person=self.person)
+            self.close()
+            self.win.show()
+
+        except Exception as er:
+            print(er)
 
 
 class Menu(QtWidgets.QMainWindow):
@@ -121,7 +180,13 @@ class Menu(QtWidgets.QMainWindow):
         self.ui.pushButton_3.clicked.connect(self.go_change)
 
     def go_zap(self):
-        pass
+        try:
+            self.win = Zap_Step1(self, person=self.person)
+            self.close()
+            self.win.show()
+
+        except Exception as er:
+            print(er)
 
     def go_watch(self):
         pass
